17ce8252601bd0f4845f4adb25ddfc93ba012c0e
Chris DeGrendele <cjdegrendele@gmail.com>
2020-07-07 15:58:17 -0500

    Initial testing problem setups

---
+++ flame_convergence/launch_lvl3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -N 2
+#SBATCH -C haswell
+#SBATCH -q regular
+#SBATCH -J Initial_MAESTRO_flamewave
+#SBATCH --mail-user=cjdegrendele@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:25:00
+
+#OpenMP settings:
+export OMP_NUM_THREADS=1
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
+
+#run the application:
+srun -n 64 -c 2 --cpu_bind=cores /global/homes/c/chrisdeg/2020/MAESTROeX/Exec/science/flame/Maestro2d.gnu.haswell.MPI.ex inputs_2d_smallscale_lvl3
